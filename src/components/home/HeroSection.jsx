37507f0aebaefead57c111a965f157f113bc43a9
JorgeAntonio <jorge.antonio.delaguila@gmail.com>
2023-09-11 21:51:09 -0500

    home page section evento añadido v3

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -5,17 +5,23 @@ const HeroSection = () => {
   return (
     <div>
       <div
-        className="hero min-h-[500px]"
+        className="hero min-h-screen bg-cover bg-center relative"
         style={{ backgroundImage: `url(${background})` }}
       >
-        <div className="hero-overlay bg-opacity-40"></div>
-        <div className="hero-content text-center text-white">
-          <div className="">
-            <h1 className="mb-5 text-7xl font-bold">¡TRAZA TU FUTURO AHORA!</h1>
-            <p className="mb-5">
-              YA ESTAS CAMINO AL EXITO, HAREMOS TUS IDEAS MAS BRILLANTES.
+        <div className="hero-overlay bg-opacity-60 absolute inset-0"></div>
+        <div className="hero-content text-center text-white relative z-10">
+          <div className="max-w-2xl mx-auto">
+            <h1 className="text-5xl md:text-7xl font-bold leading-tight mb-4">
+              ¡TRAZA TU FUTURO AHORA!
+            </h1>
+            <p className="text-lg md:text-xl text-gray-300 mb-8">
+              YA ESTÁS EN EL CAMINO DEL ÉXITO. HAREMOS QUE TUS IDEAS BRILLEN AÚN
+              MÁS.
             </p>
-            <Link to={`signup`} className="btn btn-primary">
+            <Link
+              to={`signup`}
+              className="btn btn-primary text-lg md:text-xl px-6 py-3 rounded-full hover:scale-105 transform transition-transform duration-300"
+            >
               Comenzar
             </Link>
           </div>
